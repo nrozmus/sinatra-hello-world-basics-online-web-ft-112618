e385a7f0f8cc37715814d54fdc2cfee3f698ebe7
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-01-22 02:24:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-
-end
+class App < Sinatra::Base 
+  
+  get '/' do
+   "Hello, World!"
+  end
+end 
